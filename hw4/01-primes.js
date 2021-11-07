c81b89b7dd12fafdf57f4ceeba187805b75ced7d
vengala <vengala@pdx.edu>
2021-11-07 14:40:38 -0800

    hw4 exercise1 done

--- hw4/01-primes.js
+++ hw4/01-primes.js
@@ -1,7 +1,20 @@
 const getPrimes = () => {
   // Add your code here
+  console.log("Prime numbers from 1 to 100 are:")
+  count = 0;
+  for(num = 2; num<=100; num++) {
+    for(i = 1; i <= num; i++) {
+      if(num%i == 0) {
+        count++;
+      }
+    }
+    if(count==2){
+      console.log(num);
+    }
+    count = 0;
+  }
 };
-
+   
 getPrimes();
 // 2
 // 3
